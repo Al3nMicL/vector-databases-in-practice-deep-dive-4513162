6eaae3470c4f1b02f8e362cf63676e89009f6f69
Al3nMicL <al3nmicl@users.noreply.github.com>
2025-08-08 18:09:36 -0400

    Refactor collection creation to include Google Cloud project ID in vectorizer and generative configurations

--- 02_03_create_collection.py
+++ 02_03_create_collection.py
@@ -1,9 +1,12 @@
 import utils
 import weaviate.classes as wvc
+import os
 from weaviate.classes.config import Configure, Property, DataType
 
 client = utils.connect_to_my_db()  # Connect to our own database
+google_cloud_project_id = os.getenv("GCP_PROJECT_ID") # Define this in .env to match your Google Cloud project ID
 
+print(f"Creating collection 'Movie' with Google AI Studio vectorizer... Project ID: {google_cloud_project_id}")
 client.collections.create(
     # Set the name of the collection
     name="Movie",
@@ -13,8 +16,9 @@ client.collections.create(
     vectorizer_config=Configure.Vectorizer.text2vec_google(
         model_id="models/embedding-001", # The 'text2vec_google' module uses 'model_id' to specify the model.
         api_endpoint="generative.googleapis.com",
+        project_id=google_cloud_project_id,
     ), 
-    generative_config=wvc.config.Configure.Generative.google(model="gemini-2.5-flash-lite"), # Set the generative module
+    generative_config=wvc.config.Configure.Generative.google(model_id="gemini-2.5-flash-lite", project_id=google_cloud_project_id), # Set the generative module
 
     # Define the properties of the collection
     properties=[
